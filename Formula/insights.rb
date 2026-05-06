bdd35881faa7e63b53da88a3f7c8b7b3e4492157
Blizz <blizz@kernelle-soft.com>
2026-05-06 03:36:06 +0000

    insights 0.17.2

--- Formula/insights.rb
+++ Formula/insights.rb
@@ -1,32 +1,31 @@
 class Insights < Formula
   desc "A drop-in, IDE-agnostic AI knowledge management and retrieval system"
   homepage "https://github.com/kernelle-soft/insights"
-  version "0.17.0"
+  version "0.17.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.0/insights-0.17.0-aarch64-apple-darwin.tar.gz"
-      sha256 "1930250f248d44f692d64cb2a80bf0371efaee3b92983a95eb3ad25d397e99c7"
+      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.2/insights-0.17.2-aarch64-apple-darwin.tar.gz"
+      sha256 "fa520a02af409373c3b99e1248f92b39f16c8857b970fc1fc2840051a3a465a2"
     else
-      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.0/insights-0.17.0-x86_64-apple-darwin.tar.gz"
-      sha256 "5c04b87dd3d75014f84422ce13e252fff60a373f894f4702e3b37e3e627bb7f1"
+      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.2/insights-0.17.2-x86_64-apple-darwin.tar.gz"
+      sha256 "f68935e1f113bc4b08661809a3ff5291d2d1074f67903279faa2af66396f38b6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.0/insights-0.17.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e52d84761d795488e448b3b27bf516be6c2c2bf88da3342d37f93f990dcb077b"
+      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.2/insights-0.17.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "6bf529dafd0f252e1e824789a56dfa9c977e592f3c0344174f9162e1bb2e9d7d"
     else
-      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.0/insights-0.17.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "eb9e3cb2c9f270d512c68739ccb6a9acd8186cd9532937c375486ec317acc4ec"
+      url "https://creature-workshop.kernelle-soft.com/kernelle-soft/insights/releases/download/v0.17.2/insights-0.17.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "024adb5c07447fa249626e8fcdb7d5d62dbd9e44094963307408691c046c3021"
     end
   end
 
   def install
     bin.install "insights"
-    bin.install "insights_server"
   end
 
   test do
